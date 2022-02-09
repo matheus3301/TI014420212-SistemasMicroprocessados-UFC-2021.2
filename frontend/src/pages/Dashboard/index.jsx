2ebb86b8080248f88c73dfbfb4c4fcaf73884896
Vitor Rosa <vitorosa306@gmail.com>
2022-02-09 18:21:57 -0300

    Navbar Adicionada

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import './index.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 const Dashboard = () => {
     return(
-        <h1>Teste de Organização Projeto</h1>
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand>
+                        Dashboard SmartHome
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+        </>
     )
 }
 
