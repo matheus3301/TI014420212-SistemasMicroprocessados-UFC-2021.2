90fdae761945e454cf8f25e40e75810e4b167e25
Vitor Rosa <vitorosa306@gmail.com>
2022-02-10 00:28:45 -0300

    Adicionado Labels para os Gráficos

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -3,7 +3,9 @@ import './index.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col'
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
 
 import {
     Chart as ChartJS,
@@ -29,114 +31,129 @@ Tooltip,
 Legend
 );
 
-export const options = {
-responsive: true,
-plugins: {
-    legend: {
-    position: 'top',
-    },
-    title: {
-    display: true,
-    text: 'Gráfico de Linhas',
-    },
-},
-};
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-export const data2 = {
-    labels,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Dataset 2',
-        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
-    ],
-  };
-
-export const data3 = {
-labels,
-datasets: [
-    {
-    label: 'Dataset 1',
-    data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-    borderColor: 'rgb(255, 99, 132)',
-    backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-    label: 'Dataset 2',
-    data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-    borderColor: 'rgb(53, 162, 235)',
-    backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-],
-};
-
-export const data4 = {
-labels,
-datasets: [
-    {
-    label: 'Dataset 1',
-    data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-    borderColor: 'rgb(255, 99, 132)',
-    backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-    label: 'Dataset 2',
-    data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-    borderColor: 'rgb(53, 162, 235)',
-    backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-],
-};
-
 const Dashboard = () => {
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+            position: 'top',
+            },
+        },
+    };
+
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
     const [data1, setData1] = useState({
         labels,
         datasets: [
-          {
-            label: 'Dataset 1',
-            data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-          },
-          {
-            label: 'Dataset 2',
-            data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-            borderColor: 'rgb(53, 162, 235)',
-            backgroundColor: 'rgba(53, 162, 235, 0.5)',
-          },
+            {
+                label: 'Temperatura',
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: 'Umidade',
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                borderColor: 'rgb(30, 166, 66)',
+                backgroundColor: 'rgba(30, 166, 66, 0.5)',
+            },
+            {
+                label: 'Luminosidade',
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                borderColor: 'rgb(20, 20, 219)',
+                backgroundColor: 'rgba(20, 20, 219, 0.5)',
+            },
+            {
+                label: 'Pressão',
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                borderColor: 'rgb(153, 20, 219)',
+                backgroundColor: 'rgba(153, 20, 219, 0.5)',
+            }
         ],
       })
+
+    const [data2, setData2] = useState({
+    labels,
+    datasets: [
+        {
+        label: 'Temperatura',
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+    ],
+    })
+
+    const [data3, setData3] = useState({
+        labels,
+        datasets: [
+            {
+                label: 'Umidade',
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                borderColor: 'rgb(30, 166, 66)',
+                backgroundColor: 'rgba(30, 166, 66, 0.5)',
+            },
+        ],
+        })
+
+    const [data4, setData4] = useState({
+        labels,
+        datasets: [
+            {
+                label: 'Luminosidade',
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                borderColor: 'rgb(20, 20, 219)',
+                backgroundColor: 'rgba(20, 20, 219, 0.5)',
+            }
+        ],
+        })
+
+    const [data5, setData5] = useState({
+        labels,
+        datasets: [
+            {
+                label: 'Pressão',
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                borderColor: 'rgb(153, 20, 219)',
+                backgroundColor: 'rgba(153, 20, 219, 0.5)',
+            }
+        ],
+        })
   
     useEffect(() => {
-        setInterval(function(){
+        console.log(new Date())
+        setInterval(() => {
             setData1({
                 labels,
                 datasets: [
                   {
-                    label: 'Dataset 1',
-                    data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+                    label: 'Temperatura',
+                    data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
                     borderColor: 'rgb(255, 99, 132)',
                     backgroundColor: 'rgba(255, 99, 132, 0.5)',
                   },
                   {
-                    label: 'Dataset 2',
-                    data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-                    borderColor: 'rgb(53, 162, 235)',
-                    backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                    label: 'Umidade',
+                    data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                    borderColor: 'rgb(30, 166, 66)',
+                    backgroundColor: 'rgba(30, 166, 66, 0.5)',
                   },
+                  {
+                    label: 'Luminosidade',
+                    data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                    borderColor: 'rgb(20, 20, 219)',
+                    backgroundColor: 'rgba(20, 20, 219, 0.5)',
+                  },
+                  {
+                    label: 'Pressão',
+                    data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                    borderColor: 'rgb(153, 20, 219)',
+                    backgroundColor: 'rgba(153, 20, 219, 0.5)',
+                  }
                 ],
               })
+              console.log(new Date())
         }, 60000);
     })
 
@@ -151,19 +168,68 @@ const Dashboard = () => {
             </Navbar>
             <Container className="mt-5">
                 <Row>
-                    <Col md={6}>
+                    <Col>
                         <Line options={options} data={data1} />
-                    </Col>
-                    <Col md={6}>
-                        <Line options={options} data={data2} />
                     </Col>
                 </Row>
                 <Row>
                     <Col md={6}>
-                        <Line options={options} data={data3} />
+                        <Form className="form">
+                            <Form.Group>
+                                <Form.Control type="time"/>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Control type="time"/>
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Filtrar
+                            </Button>
+                        </Form>
+                        <Line options={options} data={data2} />
                     </Col>
                     <Col md={6}>
+                        <Form className="form">
+                            <Form.Group>
+                                <Form.Control type="time"/>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Control type="time"/>
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Filtrar
+                            </Button>
+                        </Form>
+                        <Line options={options} data={data3} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={6}>
+                        <Form className="form">
+                            <Form.Group>
+                                <Form.Control type="time"/>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Control type="time"/>
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Filtrar
+                            </Button>
+                        </Form>
                         <Line options={options} data={data4} />
+                    </Col>
+                    <Col md={6}>
+                        <Form className="form">
+                            <Form.Group>
+                                <Form.Control type="time"/>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Control type="time"/>
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Filtrar
+                            </Button>
+                        </Form>
+                        <Line options={options} data={data5} />
                     </Col>
                 </Row>
             </Container>
